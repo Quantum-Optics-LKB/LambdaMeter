c513afd1b87b58d7a23bf6b1956cb7ff06d74e26
Tangui Aladjidi <taladjidi@gmail.com>
2024-08-30 13:31:38 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="LambdaMeter",
+    version="0.1",
+    description="Interfacing with HighFinesse Wavelength Meter.",
+    author="Tangui Aladjidi",
+    author_email="tangui.aladjidi@lkb.upmc.fr",
+    url="https://github.com/Quantum-Optics-LKB/LambdaMeter/tree/master",
+    packages=["LambdaMeter"],
+)
