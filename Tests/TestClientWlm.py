917ff8f9d67a4c1c7cf712b5f2ca5b15fd774106
Quentin GLX <quentin.glorieux@gmail.com>
2024-09-17 15:44:07 +0200

    update TestClientWlm.py

--- Tests/TestClientWlm.py
+++ Tests/TestClientWlm.py
@@ -6,9 +6,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from LambdaMeter.WavelengthMeterClient import WavelengthMeterClient
 
 ## host and port are the default values
-client = WavelengthMeterClient(host="localhost", port=1234)
+client = WavelengthMeterClient(host="10.0.2.103", port=1234)
 
 for i in range(10):
-    print(client.get_wavelength(2))
+    print(client.get_wavelength(4))
     # print(client.get_exposureMode(2))
     
